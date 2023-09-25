a5fa02950e88e318ea6d247abf2dfeadd8fd4aa6
Rafael Moreno <alejandro8924@gmail.com>
2023-09-25 10:45:06 -0500

    groups with text or icons

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -4,28 +4,15 @@ from libqtile.lazy import lazy
 from .keys import mod, keys
 
 
-groups = [Group(i) for i in "123456789"]
+groups = [Group(i) for i in [
+    "BROW", "DEV", "TERM", "MISC"
+]]
 
-for i in groups:
-    keys.extend(
-        [
-            # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
+for i, group in enumerate(groups):
+    actual_key = str(i + 1)
+    keys.extend([
+        # Switch to workspace N
+        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        # Send window to workspace N
+        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
+    ])
