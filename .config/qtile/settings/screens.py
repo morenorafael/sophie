417bf3fd5a1ce29c6fec788e2b031aaa4006ae94
Rafael Moreno <alejandro8924@gmail.com>
2023-09-25 17:57:46 -0500

    agregando estilos a los widgets

--- .config/qtile/settings/screens.py
+++ .config/qtile/settings/screens.py
@@ -15,19 +15,55 @@ screens = [
                 ),
                 widget.Prompt(),
                 widget.WindowName(),
+                
+                widget.TextBox(
+                    "",
+                    fontsize=30,
+                    foreground='D08770',
+                    background='2E3440',
+                    padding=0
+                    
+                ),
+
+
                 widget.CurrentLayout(
                     background='D08770'
                 ),
+
+                widget.TextBox(
+                    "",
+                    fontsize=30,
+                    foreground='BF616A',
+                    background='D08770',
+                    padding=0
+                    
+                ),
+
                 widget.Clock(
                     format="%a %I:%M %p",
                     background='BF616A'
                 ),
+
+                widget.TextBox(
+                    "",
+                    fontsize=30,
+                    foreground='2E3440',
+                    background='BF616A',
+                    padding=0
+                    
+                ),
+
                 widget.Systray(),
+
+                widget.Spacer(
+                    length=10
+                )
             ],
             24,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-            background='#2E3440'
+            background='#2E3440',
+            border_width=0
         ),
     ),
 ]
