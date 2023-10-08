d8a9fb0d87f1182fff5b15065a1c02fe246d99fa
Rafael Moreno <alejandro8924@gmail.com>
2023-10-07 21:17:25 -0500

    changed widget theme

--- .config/qtile/settings/screens.py
+++ .config/qtile/settings/screens.py
@@ -10,7 +10,7 @@ screens = [
                 widget.GroupBox(
                     # active='ECEFF4',
                     # borderwidth=0,
-                    # inactive='4C566A',
+                    # inactive='434C5E',
                     # fontsize=16,
                     # this_screen_border='ffffff'
 
@@ -19,15 +19,15 @@ screens = [
                     padding=8,
                     borderwidth=1,
                     active='ECEFF4',
-                    inactive='4C566A',
+                    inactive='434C5E',
                     rounded=False,
                     highlight_method='block',
                     urgent_alert_method='block',
                     urgent_border='D08770',
                     this_current_screen_border='81A1C1',
                     this_screen_border='2E3440',
-                    other_current_screen_border='4C566A',
-                    other_screen_border='4C566A',
+                    other_current_screen_border='434C5E',
+                    other_screen_border='434C5E',
                     disable_drag=True
                 ),
                 widget.Prompt(),
@@ -36,118 +36,69 @@ screens = [
                 
                 widget.TextBox(
                     "",
-                    fontsize=42,
-                    foreground='81A1C1',
+                    fontsize=55,
+                    foreground='434C5E',
                     background='2E3440',
-                    padding=-4
+                    padding=-6
                 ),
 
 
                 widget.CurrentLayout(
-                    background='81A1C1'
-                ),
-
-                widget.TextBox(
-                    "",
-                    fontsize=42,
-                    foreground='5E81AC',
-                    background='81A1C1',
-                    padding=-4
-                    
+                    background='434C5E',
+                    foreground='B48EAD',
+                    fmt='{} '
                 ),
 
                 widget.Battery(
-                    format='{char} {percent:2.0%} {hour:d} h {min:02d} m ',
-                    background='5E81AC',
+                    format=' {char} {percent:2.0%} {hour:d} h {min:02d} m ',
+                    background='434C5E',
+                    foreground='8FBCBB',
                     discharge_char='󱟤 ',
                     charge_char='󱟦 ',
                     low_background='BF616A',
-                    low_foreground='FFFFFF',
-                ),
-
-                widget.TextBox(
-                    "",
-                    fontsize=42,
-                    foreground='81A1C1',
-                    background='5E81AC',
-                    padding=-4
-                    
+                    low_foreground='ECEFF4',
+                    # low_percentage=0.5,
                 ),
 
                 widget.Bluetooth(
                     fmt='󰂱  {} ',
                     hci='/dev_8F_E0_2F_49_7B_5E',
-                    background='81A1C1'
-                ),
-
-                widget.TextBox(
-                    "",
-                    fontsize=42,
-                    foreground='5E81AC',
-                    background='81A1C1',
-                    padding=-4
-                    
+                    background='434C5E',
+                    foreground='88C0D0'
                 ),
 
                 widget.Wlan(
-                    format='    {essid}  {percent:2.0%}',
+                    fmt=' {}',
+                    format='  {essid}  {percent:2.0%}',
                     interface='wlp61s0',
-                    background='5E81AC'
-                ),
-
-                widget.TextBox(
-                    "",
-                    fontsize=42,
-                    foreground='81A1C1',
-                    background='5E81AC',
-                    padding=-4
-                    
+                    background='434C5E',
+                    foreground='A3BE8C'
                 ),
 
                 widget.Memory(
                     fmt='  {} ',
                     measure_mem='G',
-                    background='81A1C1'
-                ),
-
-                widget.TextBox(
-                    "",
-                    fontsize=42,
-                    foreground='5E81AC',
-                    background='81A1C1',
-                    padding=-4
-                    
+                    background='434C5E',
+                    foreground='EBCB8B'
                 ),
 
                 widget.Systray(
                     background='5E81AC',
                 ),
 
-                widget.Spacer(
-                    length=10,
-                    background='5E81AC'
-                ),
-
-                widget.TextBox(
-                    "",
-                    fontsize=42,
-                    foreground='81A1C1',
-                    background='5E81AC',
-                    padding=-4
-                    
-                ),
-
                 widget.Clock(
-                    format="%I:%M %p",
-                    background='81A1C1'
+                    fmt=' {}',
+                    format=' %I:%M %p',
+                    background='434C5E',
+                    foreground='ECEFF4'
                 ),
 
                 widget.Spacer(
                     length=10,
-                    background='81A1C1'
+                    background='434C5E'
                 )
             ],
-            24,
+            30,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
             background='#2E3440',
